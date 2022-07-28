a06269820d0097e415da2a77a4a1c3bab21a8f61
Jdvd01 <velasquezdenorajose@gmail.com>
2022-07-28 21:08:51 +0000

    [FIX] placeholder img, responsive, and webpack for deploy

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,93 +1,126 @@
-import React, { useContext } from 'react';
-import propTypes from 'prop-types';
-import { Link } from "react-router-dom"
-import { Context } from "../store/appContext"
-
+import React, { useContext } from "react";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 const Card = ({ nature, properties, uid, _id }) => {
-    const { actions } = useContext(Context);
-    const { name, gender, hair_color, eye_color, climate, gravity, population, model, passengers, cost_in_credits } = properties;
+  const { actions } = useContext(Context);
+  const {
+    name,
+    gender,
+    hair_color,
+    eye_color,
+    climate,
+    gravity,
+    population,
+    model,
+    passengers,
+    cost_in_credits,
+  } = properties;
 
-    return (
-        <>
-            {nature === "people" ? (
-                <div className="card">
-                    <img src="http://via.placeholder.com/400x200" className="card-img" alt="imagen" />
-                    <div className="card-body">
-                        <p className="card-title">{name}</p>
-                        <p className="card-text">
-                            Gender: {gender}   <br />
-                            Hair-color: {hair_color}  <br />
-                            Eye-color: {eye_color}
-                        </p>
-                        <Link to={`/${nature}/${uid}`}>
-                            <button className="btn btn-primary">  Learn More!  </button>
-                        </Link>
-                        <button
-                            type='button'
-                            className={`corazon negro`}
-                            onClick={() => actions.addFavorites(_id)}>
-                            <i className="fas fa-heart"></i>
-                        </button>
-                    </div>
-                </div>
-            ) : nature === "planets" ? (
-                <div className="card">
-                    <img src="http://via.placeholder.com/400x200" className="card-img" alt="imagen" />
-                    <div className="card-body">
-                        <p className="card-title">{name}</p>
-                        <p className="card-text">
-                            Climate: {climate}   <br />
-                            Gravity: {gravity}  <br />
-                            Population:  {population}
-                        </p>
-                        <Link to={`/${nature}/${uid}`}>
-                            <button className="btn btn-primary">  Learn More!  </button>
-                        </Link>
-                        <button
-                            className={`corazon negro`}
-                            onClick={() => actions.addFavorites(_id)}>
-                            <i className="fas fa-heart"></i>
-                        </button>
-                    </div>
-                </div>
-            ) : nature === "vehicles" ? (
-                <div className="card">
-                    <img src="http://via.placeholder.com/400x200" className="card-img" alt="imagen" />
-                    <div className="card-body">
-                        <p className="card-title">{name}</p>
-                        <p className="card-text">
-                            Model: {model}   <br />
-                            Passengers: {passengers} <br />
-                            Price: {cost_in_credits}
-                        </p>
-                        <div className="endButtons">
-                            <Link to={`/${nature}/${uid}`}>
-                                <button className="btn btn-primary">  Learn More!  </button>
-                            </Link>
-                            <button
-                                className={`corazon negro`}
-                                onClick={() => actions.addFavorites(_id)}>
-                                <i className="fas fa-heart"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <div className="d-flex align-items-center m-5">
-                    <strong>Loading...</strong>
-                    <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {nature === "people" ? (
+        <div className="card">
+          <img
+            src="https://fakeimg.pl/400x200/"
+            className="card-img"
+            alt="imagen"
+          />
+          <div className="card-body">
+            <p className="card-title">{name}</p>
+            <p className="card-text">
+              Gender: {gender} <br />
+              Hair-color: {hair_color} <br />
+              Eye-color: {eye_color}
+            </p>
+            <div className="card-footer">
+              <Link to={`/${nature}/${uid}`}>
+                <button className="btn btn-primary"> Learn More! </button>
+              </Link>
+              <button
+                type="button"
+                className={`corazon negro`}
+                onClick={() => actions.addFavorites(_id)}
+              >
+                <i className="fas fa-heart"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : nature === "planets" ? (
+        <div className="card">
+          <img
+            src="https://fakeimg.pl/400x200/"
+            className="card-img"
+            alt="imagen"
+          />
+          <div className="card-body">
+            <p className="card-title">{name}</p>
+            <p className="card-text">
+              Climate: {climate} <br />
+              Gravity: {gravity} <br />
+              Population: {population}
+            </p>
+            <div className="card-footer">
+              <Link to={`/${nature}/${uid}`}>
+                <button className="btn btn-primary"> Learn More! </button>
+              </Link>
+              <button
+                className={`corazon negro`}
+                onClick={() => actions.addFavorites(_id)}
+              >
+                <i className="fas fa-heart"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : nature === "vehicles" ? (
+        <div className="card">
+          <img
+            src="https://fakeimg.pl/400x200/"
+            className="card-img"
+            alt="imagen"
+          />
+          <div className="card-body">
+            <p className="card-title">{name}</p>
+            <p className="card-text">
+              Model: {model} <br />
+              Passengers: {passengers} <br />
+              Price: {cost_in_credits}
+            </p>
+            <div className="card-footer">
+              <Link to={`/${nature}/${uid}`}>
+                <button className="btn btn-primary"> Learn More! </button>
+              </Link>
+              <button
+                className={`corazon negro`}
+                onClick={() => actions.addFavorites(_id)}
+              >
+                <i className="fas fa-heart"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex align-items-center m-5">
+          <strong>Loading...</strong>
+          <div
+            className="spinner-border ms-auto"
+            role="status"
+            aria-hidden="true"
+          ></div>
+        </div>
+      )}
+    </>
+  );
 };
 
 Card.propTypes = {
-    nature: propTypes.string,
-    properties: propTypes.object,
-    uid: propTypes.string,
-    _id: propTypes.string
-}
+  nature: propTypes.string,
+  properties: propTypes.object,
+  uid: propTypes.string,
+  _id: propTypes.string,
+};
 
 export default Card;
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,8 @@ export const Footer = () => {
 	return(
 		<footer className="footer">
 		<p className="m-0">
-			Made with <i className="fa fa-heart text-danger" /> by Jose Velasquez a <br/>
+			Made with <i className="fa fa-heart text-danger" /> by 
+			<a target="_blank" href="https://github.com/Jdvd01"> Jose Velasquez </a> a <br/>
 			<a target="_blank" href="http://www.4geeksacademy.com"> 4Geeks Academy</a> student
 		</p>
 	</footer>
